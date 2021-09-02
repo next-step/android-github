19a1ba76d7b0443ac9339658a3976eef2320138d
강현석 <kekemusa37@gmail.com>
2021-09-02 23:39:33 +0900

    :sparkles: feat: [구현] GithubService

---
+++ data/src/main/java/com/nextstep/edu/github/data/GithubService.kt
@@ -0,0 +1,11 @@
+package com.nextstep.edu.github.data
+
+import com.nextstep.edu.github.domain.RepositoryItem
+import retrofit2.Call
+import retrofit2.http.GET
+
+internal interface GithubService {
+
+    @GET("repositories")
+    suspend fun fetchRepositories(): List<RepositoryItem>
+}
---
+++ data/src/test/java/com/nextstep/edu/github/data/GithubServiceTest.kt
@@ -0,0 +1,58 @@
+package com.nextstep.edu.github.data
+
+import com.nextstep.edu.github.domain.RepositoryItem
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import java.io.File
+import junit.framework.Assert.assertEquals
+import kotlinx.coroutines.runBlocking
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+internal class GithubServiceTest {
+
+    private val mockServer = MockWebServer()
+    private lateinit var service: GithubService
+
+    @BeforeEach
+    fun setup() {
+        service = Retrofit
+            .Builder()
+            .baseUrl(mockServer.url(""))
+            .addConverterFactory(
+                MoshiConverterFactory.create(
+                    Moshi
+                        .Builder()
+                        .add(KotlinJsonAdapterFactory())
+                        .build()
+                )
+            )
+            .build()
+            .create(GithubService::class.java)
+    }
+
+    @Test
+    fun `깃헙 레파지토리 리스트를 불러옵니다`() = runBlocking {
+        val response = MockResponse()
+            .setBody(File("src/test/resources/repositories.json").readText())
+        mockServer.enqueue(response)
+
+        val actual = service.fetchRepositories()
+
+        val expected = listOf(
+            RepositoryItem(
+                fullName = "mojombo/grit",
+                description = "**Grit is no longer maintained. Check out libgit2/rugged.** Grit gives you object oriented read/write access to Git repositories via Ruby."
+            ),
+            RepositoryItem(
+                fullName = "wycats/merb-core",
+                description = "Merb Core: All you need. None you don't."
+            )
+        )
+        assertEquals(expected, actual)
+    }
+}
