2d42e33cd7ea728a47510e6e49c78f6dcade7d44
강현석 <kekemusa37@gmail.com>
2021-09-05 01:12:20 +0900

    :sparkles: feat: [적용] GithubApplication

--- app/src/main/java/camp/nextstep/edu/github/MainActivity.kt
+++ app/src/main/java/camp/nextstep/edu/github/MainActivity.kt
@@ -1,16 +1,8 @@
 package camp.nextstep.edu.github
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
