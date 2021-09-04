f42a594792f0ba52efc457aa602cb203ebb75553
namjackson <skawo32167@naver.com>
2021-09-05 00:13:53 +0900

    feat(Step1) : Add Domain Module

---
+++ domain/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("java-library")
+    id("kotlin")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.2")
+    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.7.2") // junit4 지원
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("com.google.truth:truth:1.1.3")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
