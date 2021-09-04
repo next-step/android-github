413eeabfcc51578486ef4cee9dd36748f69d64b8
강현석 <kekemusa37@gmail.com>
2021-09-04 22:37:54 +0900

    :art: review: domain 모듈에서는 Moshi에 의존하지 않도록 해주세요.

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -9,11 +9,6 @@ java {
 }
 
 dependencies {
-    ConverterConfig.run {
-        implementation(MOSHI_KOTLIN)
-        implementation(MOSHI_KOTLIN_CODEGEN)
-    }
-
     testImplementation("org.junit.jupiter:junit-jupiter:$junit5Version")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junit5Version")
     testImplementation("org.assertj:assertj-core:3.18.0")
