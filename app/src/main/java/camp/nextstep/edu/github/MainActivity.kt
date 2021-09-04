1031841b89a10a33fb0827c95bcc782c3b596974
강현석 <kekemusa37@gmail.com>
2021-09-05 02:53:32 +0900

    :sparkles: feat: [구현] Repository 리스트 화면에 그리기

--- app/src/main/java/camp/nextstep/edu/github/MainActivity.kt
+++ app/src/main/java/camp/nextstep/edu/github/MainActivity.kt
@@ -2,13 +2,20 @@ package camp.nextstep.edu.github
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContent { ActivityMain.Layout() }
+        setContent { ActivityMain.Layout(mainViewModel = viewModel) }
+
+        viewModel.fetchRepositories()
     }
 }
