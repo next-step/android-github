c6469945baf41d2d276b2e1984d09e33f9b9b839
강현석 <kekemusa37@gmail.com>
2021-09-05 00:37:45 +0900

    :sparkles: feat: [구현] NetworkModule

---
+++ data/src/main/java/com/nextstep/edu/github/data/di/NetworkModule.kt
@@ -0,0 +1,55 @@
+package com.nextstep.edu.github.data.di
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetworkModule {
+
+    @Provides
+    @Singleton
+    fun providesKotlinJsonAdapterFactory(): KotlinJsonAdapterFactory = KotlinJsonAdapterFactory()
+
+    @Provides
+    @Singleton
+    fun providesMoshi(jsonAdapter: KotlinJsonAdapterFactory): Moshi = Moshi
+        .Builder()
+        .add(jsonAdapter)
+        .build()
+
+    @Provides
+    @Singleton
+    fun providesMoshiConverter(moshi: Moshi): MoshiConverterFactory =
+        MoshiConverterFactory.create(moshi)
+
+    @Provides
+    @Singleton
+    fun providesOkHttpClient(): OkHttpClient = OkHttpClient
+        .Builder()
+        .build()
+
+    @Provides
+    @Singleton
+    fun providesRetrofit(
+        converterFactory: MoshiConverterFactory,
+        client: OkHttpClient
+    ): Retrofit = Retrofit
+        .Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(converterFactory)
+        .client(client)
+        .build()
+
+    companion object {
+        private const val BASE_URL = "https://api.github.com/"
+    }
+}
