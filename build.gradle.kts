c3b1a1dbbb93a10a4ee61748ace838fa82a7838b
Malibin <nightmare73@naver.com>
2021-09-13 22:39:26 +0900

    [Refactor] Gradle.kts dsl 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
         classpath("org.jlleitschuh.gradle:ktlint-gradle:10.0.0")
         classpath("de.mannodermaus.gradle.plugins:android-junit5:1.7.1.1")
+        classpath(Hilt.androidGradlePlugin)
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
--- buildSrc/src/main/kotlin/Configs.kt
+++ buildSrc/src/main/kotlin/Configs.kt
@@ -1,6 +1,18 @@
 const val kotlinVersion = "1.5.21"
 const val junit5Version = "5.7.2"
 
+object Androidx {
+    const val core = "androidx.core:core-ktx:1.6.0"
+    const val appcompat = "androidx.appcompat:appcompat:1.3.1"
+    const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.1.0"
+    const val fragmentKtx = "androidx.fragment:fragment-ktx:1.3.3"
+    const val googleMaterial ="com.google.android.material:material:1.4.0"
+}
+
+object Kotlin {
+    const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion"
+}
+
 object Hilt {
     private const val version = "2.38.1"
 
@@ -31,8 +43,12 @@ object Coroutine {
 object Test {
     const val junit4 = "junit:junit:4.13.2"
     const val junit5 = "org.junit.jupiter:junit-jupiter:5.7.2"
-    const val androidxJunit = "androidx.test.ext:junit:1.1.2"
-    const val espressoCore = "androidx.test.espresso:espresso-core:3.3.0"
+    const val junitVintageEngine = "org.junit.vintage:junit-vintage-engine:$junit5Version"
+    const val androidxJunit = "androidx.test.ext:junit:1.1.3"
+    const val espressoCore = "androidx.test.espresso:espresso-core:3.4.0"
+
+    const val androidTestCore = "de.mannodermaus.junit5:android-test-core:1.2.2"
+    const val androidTestRunner = "de.mannodermaus.junit5:android-test-runner:1.2.2"
 
     const val truth = "com.google.truth:truth:1.1.2"
     const val mockk = "io.mockk:mockk:1.12.0"
