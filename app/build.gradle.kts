3bc10f7694fd6de28eff4adca82945fdff86b367
devsoupe <luvyum4e@gmail.com>
2022-02-25 15:24:11 +0900

    feat : Kotlin DSL depencency 정리

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,19 +43,29 @@ android {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
-    implementation("androidx.core:core-ktx:1.6.0")
-    implementation("androidx.appcompat:appcompat:1.3.1")
-    implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+    Kotlin.apply {
+        implementation(STDLIB)
+    }
 
-    testImplementation("org.junit.jupiter:junit-jupiter:$junit5Version")
-    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:$junit5Version") // junit4 지원
-    testImplementation("junit:junit:4.13.2")
-    testImplementation("com.google.truth:truth:1.1.3")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-    androidTestImplementation("org.junit.jupiter:junit-jupiter-api:$junit5Version")
-    androidTestImplementation("de.mannodermaus.junit5:android-test-core:1.2.2")
-    androidTestRuntimeOnly("de.mannodermaus.junit5:android-test-runner:1.2.2")
+    Androidx.apply {
+        implementation(CORE)
+        implementation(APPCOMPAT)
+        implementation(GOOGLE_MATERIAL)
+        implementation(CONSTRAINT_LAYOUT)
+    }
+
+    Test.apply {
+        testImplementation(JUNIT_JUPITER)
+        testRuntimeOnly(JUNIT_VINTAGE_ENGINE) // junit4 지원
+        testImplementation(JUNIT)
+        testImplementation(THRUTH)
+    }
+
+    AndroidTest.apply {
+        androidTestImplementation(JUNIT)
+        androidTestImplementation(ESSPRESSO_CORE)
+        androidTestImplementation(JUNIT_JUPITER_API)
+        androidTestImplementation(ANDROID_TEST_CORE)
+        androidTestRuntimeOnly(ANDROID_TEST_RUNNER)
+    }
 }
--- buildSrc/src/main/kotlin/Configs.kt
+++ buildSrc/src/main/kotlin/Configs.kt
@@ -1,2 +1,30 @@
-const val kotlinVersion = "1.5.21"
-const val junit5Version = "5.7.2"
+const val KOTLIN_VERSION = "1.5.21"
+const val JUNIT5_VERSION = "5.7.2"
+
+object Kotlin {
+    const val STDLIB = "org.jetbrains.kotlin:kotlin-stdlib:$KOTLIN_VERSION"
+}
+
+object Androidx {
+    const val CORE = "androidx.core:core-ktx:1.6.0"
+    const val APPCOMPAT = "androidx.appcompat:appcompat:1.3.1"
+    const val CONSTRAINT_LAYOUT = "androidx.constraintlayout:constraintlayout:2.1.3"
+    const val GOOGLE_MATERIAL = "com.google.android.material:material:1.4.0"
+}
+
+object Test {
+    const val JUNIT_JUPITER = "org.junit.jupiter:junit-jupiter:$JUNIT5_VERSION"
+    const val JUNIT_VINTAGE_ENGINE = "org.junit.vintage:junit-vintage-engine:$JUNIT5_VERSION"
+    const val JUNIT = "junit:junit:4.13.2"
+    const val THRUTH = "com.google.truth:truth:1.1.3"
+}
+
+object AndroidTest {
+    private const val DE_MANNODERMAUS_JUNIT5 = "1.2.2"
+
+    const val JUNIT = "androidx.test.ext:junit:1.1.3"
+    const val ESSPRESSO_CORE = "androidx.test.espresso:espresso-core:3.4.0"
+    const val JUNIT_JUPITER_API = "org.junit.jupiter:junit-jupiter-api:$JUNIT5_VERSION"
+    const val ANDROID_TEST_CORE = "de.mannodermaus.junit5:android-test-core:$DE_MANNODERMAUS_JUNIT5"
+    const val ANDROID_TEST_RUNNER = "de.mannodermaus.junit5:android-test-runner:$DE_MANNODERMAUS_JUNIT5"
+}
