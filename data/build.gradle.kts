81750d87a0da09a0f433a4076d25732019efa54a
DoDoBest <av449@naver.com>
2022-02-27 23:21:00 +0900

    build: Data 모듈에 OkHttp3, MockWebServer 의존성 추가

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -11,4 +11,7 @@ java {
 dependencies {
     implementation(project(":domain"))
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.9.3")
+
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.9.3")
 }
