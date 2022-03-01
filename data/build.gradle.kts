54eefe42277f3e94f1fc9a45908c096e31c5d8b9
chohohee <gone1005@gmail.com>
2022-03-01 18:15:10 +0900

    build: retrofit, gson, test 추가

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -10,4 +10,12 @@ java {
 
 dependencies {
     implementation(project(":domain"))
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.google.code.gson:gson:2.9.0")
+
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.9.3")
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("com.google.truth:truth:1.1.3")
 }
