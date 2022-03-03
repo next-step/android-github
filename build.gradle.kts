2a73764d365559449ed74858fa63e6bc85e67094
ganghee <idiosauri@naver.com>
2022-03-03 23:42:24 +0900

    feat: dependencies 구현

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,11 +6,11 @@ buildscript {
         maven("https://plugins.gradle.org/m2/")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.1.1")
+        classpath("com.android.tools.build:gradle:7.1.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
         classpath("org.jlleitschuh.gradle:ktlint-gradle:10.2.1")
         classpath("de.mannodermaus.gradle.plugins:android-junit5:1.7.1.1")
-
+        classpath("org.jetbrains.kotlin:kotlin-serialization:1.6.10")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java-library")
     id("kotlin")
+    id("kotlinx-serialization")
 }
 
 java {
@@ -10,4 +11,15 @@ java {
 
 dependencies {
     implementation(project(":domain"))
+
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.google.code.gson:gson:2.9.0")
+    implementation("com.squareup.okhttp3:mockwebserver:4.9.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.0")
+
+    testImplementation("org.junit.jupiter:junit-jupiter:$junit5Version")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.0")
+    testImplementation("com.google.truth:truth:1.1.3")
+    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:$junit5Version")
 }
