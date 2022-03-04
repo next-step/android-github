bcb3ccb06711a7e3947586f1eab3af4be2ccfe03
dlwls5201 <dlwls5201@naver.com>
2022-03-04 23:00:51 +0900

    step2 data build gradle 수정

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,12 +1,40 @@
 plugins {
     id("java-library")
-    id("org.jetbrains.kotlin.jvm")
+    id("kotlin")
+    id("kotlin-kapt")
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
 }
+
+tasks.test {
+    useJUnitPlatform()
+}
+
 dependencies {
     implementation(project(":domain"))
+
+    Coroutine.apply {
+        implementation(CORE)
+        implementation(TEST)
+    }
+
+    Network.apply {
+        implementation(RETROFIT)
+        implementation(CONVERTER_GSON)
+        implementation(MOSHI_KOTLIN)
+        implementation(MOSHI_KOTLIN_CODEGEN)
+        implementation(OKHTTP)
+        testImplementation(MOCK_WEB_SERVER)
+    }
+
+    Test.apply {
+        testImplementation(JUNIT5_JUPITER)
+        testImplementation(TRUTH)
+        testImplementation(MOCKK)
+
+        testRuntimeOnly(JUNIT5_VINTAGE_ENGINE)
+    }
 }
