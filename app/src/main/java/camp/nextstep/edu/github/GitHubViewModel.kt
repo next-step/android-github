140d8216bb495c1ba3a271dbfac460ec3bd5e20f
Pyro <rhwjddhks@gmail.com>
2022-03-05 14:43:58 +0900

    refactor: UseCase 가 invoke 하나의 함수만 오버로딩 하도록 수정

--- app/src/main/java/camp/nextstep/edu/github/GitHubViewModel.kt
+++ app/src/main/java/camp/nextstep/edu/github/GitHubViewModel.kt
@@ -5,21 +5,20 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import camp.nextstep.edu.github.domain.model.GitHub
-import camp.nextstep.edu.github.domain.usecase.GitHubUseCase
+import camp.nextstep.edu.github.domain.usecase.FetchGitHubUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class GitHubViewModel @Inject constructor(
-    private val gitHubUseCase: GitHubUseCase
+    private val fetchGitHubUseCase: FetchGitHubUseCase
 ) : ViewModel() {
     private val _githubs = MutableLiveData<List<GitHub>>()
     val githubs: LiveData<List<GitHub>> = _githubs
 
     fun fetch() = viewModelScope.launch {
-        gitHubUseCase
-            .fetch()
+        fetchGitHubUseCase()
             .onSuccess { _githubs.value = it }
     }
 }
