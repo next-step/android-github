d9c824c32085a9cc96aec65cd24a19d2f7aff671
dlwls5201 <dlwls5201@naver.com>
2022-03-07 18:05:22 +0900

    step3 RecyclerViewExt 생성

---
+++ app/src/main/java/camp/nextstep/edu/github/RecyclerViewExt.kt
@@ -0,0 +1,29 @@
+package camp.nextstep.edu.github
+
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ *  itemLayoutId="@{@layout/item_sample}"
+ *  itemList="@{items}"
+ */
+@BindingAdapter("itemList", "itemLayoutId")
+fun <VM : Any> RecyclerView.bindListAdapterData(
+    itemList: List<VM>?,
+    layoutId: Int
+) {
+    if (itemList == null) return
+
+    if (layoutManager == null) {
+        layoutManager = LinearLayoutManager(context)
+    }
+
+    val simpleLisAdapter = adapter as? SimpleListAdapter<VM>
+        ?: object : SimpleListAdapter<VM>(
+            layoutRes = layoutId
+        ) {}
+
+    adapter = simpleLisAdapter
+    simpleLisAdapter.replaceAll(itemList)
+}
