6297f29a01e34d0f2f46d76724ca4833f3d16728
junpilpark <iamfeel2@gmail.com>
2022-03-07 15:02:39 +0900

    feat: domain, data 모듈의 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+    implementation(project(mapOf("path" to ":domain")))
 
     testImplementation("org.junit.jupiter:junit-jupiter:$junit5Version")
     testRuntimeOnly("org.junit.vintage:junit-vintage-engine:$junit5Version") // junit4 지원
