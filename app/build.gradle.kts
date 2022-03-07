b6396a4a8bc4c9340d3a155e2559a324e21deafc
jinsu4755 <parkjinsu4755@gmail.com>
2022-03-07 15:58:24 +0900

    build(step1): Config.kt 에 정의된 내용으로 build.gradle.kts 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,19 +43,34 @@ android {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
-    implementation("androidx.core:core-ktx:1.6.0")
-    implementation("androidx.appcompat:appcompat:1.3.1")
-    implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+    implementation(project(":domain"))
 
-    testImplementation("org.junit.jupiter:junit-jupiter:$junit5Version")
-    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:$junit5Version") // junit4 지원
-    testImplementation("junit:junit:4.13.2")
-    testImplementation("com.google.truth:truth:1.1.3")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-    androidTestImplementation("org.junit.jupiter:junit-jupiter-api:$junit5Version")
-    androidTestImplementation("de.mannodermaus.junit5:android-test-core:1.2.2")
-    androidTestRuntimeOnly("de.mannodermaus.junit5:android-test-runner:1.2.2")
+    Kotlin.run {
+        implementation(stdlib)
+    }
+
+    AndroidX.run {
+        implementation(core)
+        implementation(appcompat)
+        implementation(constraintLayout)
+    }
+
+    Google.run {
+        implementation(material)
+    }
+
+    Test.run {
+        testImplementation(junit4)
+        testImplementation(junit5)
+        testRuntimeOnly(junitVintageEngine)
+
+        testImplementation(truth)
+
+        androidTestImplementation(espresso)
+        androidTestImplementation(androidxJunit)
+
+        androidTestImplementation(junit5JupiterApi)
+        androidTestImplementation(androidTestCore)
+        androidTestRuntimeOnly(androidTestRunner)
+    }
 }
