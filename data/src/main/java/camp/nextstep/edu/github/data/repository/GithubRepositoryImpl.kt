294e47d86375eedf141bedb1bd77c667ec08b943
dlwls5201 <dlwls5201@naver.com>
2022-03-07 17:51:29 +0900

    step3 RepositoryModule 추가

--- data/src/main/java/camp/nextstep/edu/github/data/repository/GithubRepositoryImpl.kt
+++ data/src/main/java/camp/nextstep/edu/github/data/repository/GithubRepositoryImpl.kt
@@ -2,14 +2,14 @@ package camp.nextstep.edu.github.data.repository
 
 import camp.nextstep.edu.github.data.remote.model.mapToDomain
 import camp.nextstep.edu.github.data.remote.service.GithubService
-import camp.nextstep.edu.github.domain.model.Repository
+import camp.nextstep.edu.github.domain.model.Repositories
 import camp.nextstep.edu.github.domain.repository.GithubRepository
 
 internal class GithubRepositoryImpl(
     private val githubService: GithubService
 ) : GithubRepository {
-    override suspend fun getRepositories(): List<Repository> {
+    override suspend fun getRepositories(): Repositories {
         val result = githubService.getRepositories()
-        return result.map { it.mapToDomain() }
+        return Repositories(result.map { it.mapToDomain() })
     }
 }
