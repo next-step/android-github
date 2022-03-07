1940962f6bd7787f55a4d3407e6243077eab7235
Jinsu Park <parkjinsu4755@gmail.com>
2022-03-07 20:06:48 +0900

    Step1 - GitHub (모듈 분리) (#24)
    * build(step1): kotlin DSL 사용을 위한 의존성 정리
    
    * feat(step1): domain 모듈 작성
    
    * feat(step1): data 모듈 작성
    
    * build(step3): 추가된 모듈 settings.gradle.kts 에 추가
    
    * build(step1): Config.kt 에 정의된 내용으로 build.gradle.kts 수정
    
    * build(step1): targetSdk 변경
    
    - targetSdk 및 complieSdk 를 30 에서 31로 변경

--- buildSrc/src/main/kotlin/Configs.kt
+++ buildSrc/src/main/kotlin/Configs.kt
@@ -1,2 +1,33 @@
-const val kotlinVersion = "1.5.21"
 const val junit5Version = "5.7.2"
+
+object AndroidX {
+    const val core = "androidx.core:core-ktx:1.7.0"
+    const val appcompat = "androidx.appcompat:appcompat:1.4.1"
+    const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.1.3"
+}
+
+object Google {
+    const val material = "com.google.android.material:material:1.5.0"
+}
+
+object Kotlin {
+    const val version = "1.6.10"
+    const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib:$version"
+}
+
+object Test {
+    private const val junit5Version = "5.8.2"
+    private const val androidJunit5Version = "1.3.0"
+
+    const val junit4 = "junit:junit:4.13.2"
+    const val androidxJunit = "androidx.test.ext:junit:1.1.3"
+    const val espresso = "androidx.test.espresso:espresso-core:3.4.0"
+
+    const val junit5 = "org.junit.jupiter:junit-jupiter:$junit5Version"
+    const val junitVintageEngine = "org.junit.vintage:junit-vintage-engine:$junit5Version"
+    const val junit5JupiterApi = "org.junit.jupiter:junit-jupiter-api:$junit5Version"
+    const val androidTestCore = "de.mannodermaus.junit5:android-test-core:$androidJunit5Version"
+    const val androidTestRunner = "de.mannodermaus.junit5:android-test-runner:$androidJunit5Version"
+
+    const val truth = "com.google.truth:truth:1.1.3"
+}
---
+++ domain/src/main/java/camp/nextstep/edu/domain/MyClass.kt
@@ -0,0 +1,4 @@
+package camp.nextstep.edu.domain
+
+class MyClass {
+}
