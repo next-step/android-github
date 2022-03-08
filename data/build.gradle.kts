d4b884b458427b09ea52545df114ae1908488fb9
Junpil Park <iamfeel@gmail.com>
2022-03-08 12:47:33 +0900

    feat: JavaVersion.VERSION_1_8 으로 수정

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_7
-    targetCompatibility = JavaVersion.VERSION_1_7
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 dependencies {
     implementation(project(mapOf("path" to ":domain")))
