c5e4682af07e364121595a60fbe05b2137235f5e
Woogear Kwon <techkwon1717@hanmail.net>
2022-03-12 01:45:52 +0900

    [Test] GithubRepositoryImplTest 테스트 케이스 작성

--- data/src/test/java/camp/nextstep/edu/github/data/GithubRepositoryImplTest.kt
+++ data/src/test/java/camp/nextstep/edu/github/data/GithubRepositoryImplTest.kt
@@ -1,7 +1,10 @@
 package camp.nextstep.edu.github.data
 
+import camp.nextstep.edu.github.domain.GithubRepoModel
 import camp.nextstep.edu.github.domain.GithubRepository
-import io.mockk.coVerify
+import com.google.common.truth.Truth.assertThat
+import io.mockk.coEvery
+import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -10,15 +13,39 @@ internal class GithubRepositoryImplTest {
 
     private lateinit var repository: GithubRepository
 
+
     @BeforeEach
     fun setUp() {
-        repository = GithubRepositoryImpl()
+        repository = mockk(relaxed = true)
     }
 
     @Test
-    fun `깃헙 repository 리스트를 불러온다`() = runBlocking {
-        coVerify {
-            repository.fetchGithubRepos()
-        }
+    fun `깃헙 repository 리스트 호출을 성공한다`() = runBlocking {
+        // given
+        // when
+        val mockResult = Result.success(
+            listOf(
+                GithubRepoModel("testName1", "testDesc1"),
+                GithubRepoModel("testName2", "testDesc2")
+            )
+        )
+
+        coEvery { repository.fetchGithubRepos() } returns mockResult
+
+        // then
+        assertThat(mockResult.isSuccess).isTrue()
+        val repos = mockResult.getOrNull()
+        assertThat(repos).isNotNull()
+    }
+
+    @Test
+    internal fun `'깃헙 repository 리스트 호출에 실패한다`() {
+        // given
+        // when
+        val mockResult = Result.failure<List<GithubRepoModel>>(Exception())
+        coEvery { repository.fetchGithubRepos() } returns mockResult
+
+        // then
+        assertThat(mockResult.isSuccess).isFalse()
     }
 }
