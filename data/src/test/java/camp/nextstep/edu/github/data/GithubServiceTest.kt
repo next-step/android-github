29a4b9fd89c5a94ba7ac404bfd8b82210472d947
Woogear Kwon <techkwon1717@hanmail.net>
2022-03-12 00:59:21 +0900

    [Test] GithubServiceTest 테스트 케이스 추가

--- data/src/test/java/camp/nextstep/edu/github/data/GithubServiceTest.kt
+++ data/src/test/java/camp/nextstep/edu/github/data/GithubServiceTest.kt
@@ -49,4 +49,36 @@ class GithubServiceTest {
         // then
         assertThat(actualData).isEqualTo(expected)
     }
+
+    @Test
+    internal fun `깃헙 레포지토리 리스트를 호출했지만 응답값이 비어있다`() = runBlocking {
+        // given
+        val jsonResponse = MockResponse()
+            .setBody(File("src/test/resources/github_fetch_repos_200_empty.json").readText())
+        mockServer.enqueue(jsonResponse)
+
+        // when
+        val response = service.fetchGithubRepos()
+        val actualData = response.body()
+
+        val expected = listOf<GithubRepoData>()
+
+        // then
+        assertThat(actualData).isEqualTo(expected)
+    }
+
+    @Test
+    internal fun `깃헙 레포지토리 리스트를 호출했지만 응답 에러가 발생한다`() = runBlocking {
+        // given
+        val jsonResponse = MockResponse()
+            .setBody(File("src/test/resources/github_fetch_repos_404.json").readText())
+            .setResponseCode(404)
+        mockServer.enqueue(jsonResponse)
+
+        // when
+        val actualResponse = service.fetchGithubRepos()
+
+        // then
+        assertThat(actualResponse.isSuccessful).isFalse()
+    }
 }
