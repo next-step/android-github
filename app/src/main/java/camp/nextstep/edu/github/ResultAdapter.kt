1c5b98ccfde745abfe0d0b32deea5647e98bdc8d
DoDoBest <av449@naver.com>
2022-03-13 01:43:58 +0900

    feat: RecyclerView에 사용할 Adapter 만들기

---
+++ app/src/main/java/camp/nextstep/edu/github/ResultAdapter.kt
@@ -0,0 +1,25 @@
+package camp.nextstep.edu.github
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import camp.nextstep.edu.github.databinding.ItemRepoBinding
+import com.github.dodobest.domain.GithubData
+
+class ResultAdapter : RecyclerView.Adapter<ResultAdapterViewHolder>() {
+    private var results: List<GithubData> = emptyList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ResultAdapterViewHolder {
+        val binding: ItemRepoBinding = ItemRepoBinding.inflate(LayoutInflater.from(parent.context))
+        return ResultAdapterViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holderResultAdapter: ResultAdapterViewHolder, position: Int) {
+        holderResultAdapter.fullName.text = results[position].full_name
+        holderResultAdapter.description.text = results[position].description
+    }
+
+    override fun getItemCount(): Int {
+        return results.size
+    }
+}
