2742657f20ce91b9d6e4d8b329c881869faa939d
DoDoBest <av449@naver.com>
2022-03-12 14:06:34 +0900

    refactor: GithubRepository GithubData로 이름 변경, 카멜 케이스 이용

---
+++ domain/src/main/java/com/github/dodobest/domain/GithubData.kt
@@ -0,0 +1,3 @@
+package com.github.dodobest.domain
+
+data class GithubData(val fullName: String, val description: String)
--- domain/src/main/java/com/github/dodobest/domain/GithubRepository.kt
+++
@@ -1,3 +0,0 @@
-package com.github.dodobest.domain
-
-data class GithubRepository(val full_name: String, val description: String)
