3c58ffd956c7e1a0aa8e6626f085d5adea4249c5
DoDoBest <av449@naver.com>
2022-03-12 14:07:05 +0900

    refactor: domain 모듈에 Retrofit 관련 클래스 삭제

--- domain/src/main/java/com/github/dodobest/domain/RetrofitClient.kt
+++
@@ -1,5 +0,0 @@
-package com.github.dodobest.domain
-
-interface RetrofitClient {
-    fun getRetrofit(baseUrl: String): MyRetrofit
-}
--- domain/src/main/java/com/github/dodobest/domain/RetrofitManager.kt
+++
@@ -1,5 +0,0 @@
-package com.github.dodobest.domain
-
-interface RetrofitManager {
-    fun getRepositories() : List<GithubRepository>
-}
