dd80cd439cced7ea8b72afaf58253aa56ea5dde6
DoDoBest <av449@naver.com>
2022-03-12 16:52:54 +0900

    feat: viewModel과 xml 바인딩

--- app/src/main/java/camp/nextstep/edu/github/GithubViewModel.kt
+++ app/src/main/java/camp/nextstep/edu/github/GithubViewModel.kt
@@ -1,10 +1,25 @@
 package camp.nextstep.edu.github
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.github.dodobest.domain.GithubData
 import com.github.dodobest.domain.usecase.GetRepositoriesUseCase
 
 class GithubViewModel(
     private val getRepositoriesUseCase: GetRepositoriesUseCase
 ) : ViewModel() {
+    private val _githubData = MutableLiveData<List<GithubData>>(emptyList())
+        val githubData: LiveData<List<GithubData>>
+            get() = _githubData
+
+    private val _gitText = MutableLiveData<String>()
+        val gitText: LiveData<String>
+            get() = _gitText
+
+    fun getRepositories() {
+        _githubData.value = getRepositoriesUseCase()
+        _gitText.value = _githubData.value.toString()
+    }
 
 }
