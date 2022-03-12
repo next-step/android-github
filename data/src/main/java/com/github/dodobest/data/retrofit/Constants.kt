16dd86b3b2499696e620e459e6bc561c6e53c9b2
DoDoBest <av449@naver.com>
2022-03-13 02:00:31 +0900

    refactor: 사용하지 않는 변수 삭제

--- data/src/main/java/com/github/dodobest/data/retrofit/Constants.kt
+++ data/src/main/java/com/github/dodobest/data/retrofit/Constants.kt
@@ -3,5 +3,4 @@ package com.github.dodobest.data.retrofit
 object API {
     const val BASE_URL : String = "https://api.github.com/"
     const val SEARCH_PUBLIC_REPOSITORY: String = "repositories"
-    const val RESPONSE_OK : Int = 200
 }
