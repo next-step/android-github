de9c54111ff4e71bb8d55e0c17408a19f2178ec5
Woogear Kwon <techkwon1717@hanmail.net>
2022-03-15 22:48:44 +0900

    [REFACTOR] 피드백반영: 로딩관련 singleLiveEvent -> LiveData 로 변경

--- app/src/main/java/camp/nextstep/edu/github/GithubViewModel.kt
+++ app/src/main/java/camp/nextstep/edu/github/GithubViewModel.kt
@@ -1,6 +1,7 @@
 package camp.nextstep.edu.github
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import camp.nextstep.edu.github.domain.GithubRepoModel
@@ -17,25 +18,25 @@ class GithubViewModel @Inject constructor(
     private val _reposEvent = SingleLiveEvent<List<GithubRepoModel>>()
     val reposEvent: LiveData<List<GithubRepoModel>> get() = _reposEvent
 
-    private val _loadingEvent = SingleLiveEvent<Boolean>()
-    val loadingEvent: LiveData<Boolean> get() = _loadingEvent
+    private val _loadingState = MutableLiveData<Boolean>()
+    val loadingState: LiveData<Boolean> get() = _loadingState
 
     init {
         fetchGithubRepos()
     }
 
     private fun fetchGithubRepos() = viewModelScope.launch {
-        _loadingEvent.value = true
+        _loadingState.value = true
 
         val result = githubRepository.fetchGithubRepos()
         if (result.isSuccess) {
             _reposEvent.postValue(
                 result.getOrElse { emptyList() }
             )
-            _loadingEvent.postValue(false)
+            _loadingState.postValue(false)
             return@launch
         }
 
-        _loadingEvent.postValue(false)
+        _loadingState.postValue(false)
     }
 }
