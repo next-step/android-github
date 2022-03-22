9759205757edb063a8f6331ffc98d62282f5486d
DoDoBest <av449@naver.com>
2022-03-22 14:58:24 +0900

    refactor: RetrofitManagerImpl internal class로 만들기

--- data/src/main/java/com/github/dodobest/data/retrofit/RetrofitManagerImpl.kt
+++ data/src/main/java/com/github/dodobest/data/retrofit/RetrofitManagerImpl.kt
@@ -7,7 +7,7 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 
-class RetrofitManagerImpl(
+internal class RetrofitManagerImpl(
     private val retrofit : Retrofit
 ) : RetrofitManager {
     override fun getRepositories(onSuccess: (List<GithubData>) -> Unit, onFailure: (Throwable) -> Unit) {
