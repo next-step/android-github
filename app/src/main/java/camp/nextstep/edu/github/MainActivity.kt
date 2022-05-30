4c24875e723d5fd9cf5ad1a1609313518608a3ff
simuelunbo <simuelunbo@gmail.com>
2022-05-30 18:31:16 +0900

    feat: activityMain 생성

--- app/src/main/java/camp/nextstep/edu/github/MainActivity.kt
+++ app/src/main/java/camp/nextstep/edu/github/MainActivity.kt
@@ -1,11 +1,42 @@
 package camp.nextstep.edu.github
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import camp.nextstep.edu.github.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by viewModels()
+    private val githubAdapter: GithubAdapter by lazy {
+        GithubAdapter()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initAdapter()
+        viewModelCallback()
+        viewModel.requestGithubRepository()
     }
+
+    private fun viewModelCallback() {
+        viewModel.githubList.observe(this) { githubList ->
+            githubAdapter.submitList(githubList)
+        }
+        viewModel.showErrorMessage.observe(this) {
+            if (it.consumed) return@observe
+            Toast.makeText(this, it.consume(), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun initAdapter() {
+        binding.recyclerView.adapter = githubAdapter
+    }
+
+
 }
