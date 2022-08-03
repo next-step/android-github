3f868e8801eab4a126e9340871e54dd59a6250eb
dla3946gns <58911609+dla3946gns@users.noreply.github.com>
2022-08-04 00:34:37 +0900

    1단계 Github 모듈 분리 (#40)

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,13 +6,10 @@ buildscript {
         maven("https://plugins.gradle.org/m2/")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0")
+        classpath("com.android.tools.build:gradle:7.2.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("org.jlleitschuh.gradle:ktlint-gradle:10.0.0")
         classpath("de.mannodermaus.gradle.plugins:android-junit5:1.7.1.1")
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
     }
 }
 
