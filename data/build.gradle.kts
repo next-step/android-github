36cd98f1cf2dc012e7a087b79799a07ee4a2fb82
Keich.Park <svclaw2000@gmail.com>
2022-08-03 21:37:43 +0900

    1단계 - GitHub (모듈 분리) (#42)
    * build: domain 모듈 추가
    
    * build: data 모듈 추가
    
    * build: 테스트 관련 의존성 추가

---
+++ data/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("kotlin")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+dependencies {
+    implementation(KotlinStdLib)
+
+    testImplementation(JunitJupyter)
+    testRuntimeOnly(JunitVintageEngine) // junit4 지원
+    testImplementation(Junit4)
+    testImplementation(Truth)
+
+    implementation(project(":domain"))
+}
