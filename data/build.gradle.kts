da85091f72196c75d2cfd6f2148c5d635e932aa1
greedy0110 <greedy0110@gmail.com>
2022-08-04 22:15:24 +0900

    서버 통신 관련 의존성 추가

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("kotlin")
+    id("com.google.devtools.ksp").version("1.6.10-1.0.4")
 }
 
 repositories {
@@ -7,10 +8,15 @@ repositories {
 }
 
 dependencies {
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.moshi:moshi:1.13.0")
+    ksp("com.squareup.moshi:moshi-kotlin-codegen:1.13.0")
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.10.0")
+
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("com.google.truth:truth:1.1.3")
     testImplementation("io.mockk:mockk:1.12.3")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:$junit5Version")
 }
 
 tasks.withType<Test> {
