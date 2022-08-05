4f75930928c15d953ad25f597a73d4e40ee53990
AnKyungMoo <sodp2@naver.com>
2022-08-06 03:20:43 +0900

    feat: 레이아웃들에 databinding 설정

--- app/src/main/java/camp/nextstep/edu/github/ui/MainActivity.kt
+++ app/src/main/java/camp/nextstep/edu/github/ui/MainActivity.kt
@@ -4,15 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import camp.nextstep.edu.github.R
+import camp.nextstep.edu.github.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         viewModel.getRepositories()
     }
