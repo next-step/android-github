5839b307f169e7e1fd94049d86f4b34a0c604185
noah.choung <noah.choung@kakaocorp.com>
2022-08-05 17:49:56 +0900

    repository 인터페이스 구현

--- data/src/main/java/camp/nextstep/edu/github/data/DefaultGithubRepository.kt
+++ data/src/main/java/camp/nextstep/edu/github/data/DefaultGithubRepository.kt
@@ -1,12 +1,14 @@
 package camp.nextstep.edu.github.data
 
+import camp.nextstep.edu.github.domain.GithubRepository
+
 /**
  * 클래스에 대한 간단한 설명이나 참고 url을 남겨주세요.
  * Created by jeongjinhong on 2022. 08. 04..
  */
-internal class GithubRepositoryImpl {
+internal class DefaultGithubRepository : GithubRepository {
     private val apiService = ApiServiceFactory.apiService
-    suspend fun getGithub(): List<GithubResponse> {
+    override suspend fun getGithub(): List<GithubResponse> {
         return apiService.getGithub()
     }
 }
--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -7,3 +7,6 @@ java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
 }
+dependencies {
+    implementation(project(mapOf("path" to ":data")))
+}
