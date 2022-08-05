02d30c2d95312bab424e7f605901bc9158bfc615
AnKyungMoo <sodp2@naver.com>
2022-08-06 03:30:59 +0900

    feat: domainModel to uiModel

--- app/src/main/java/camp/nextstep/edu/github/model/GithubRepoUiModel.kt
+++ app/src/main/java/camp/nextstep/edu/github/model/GithubRepoUiModel.kt
@@ -1,6 +1,15 @@
 package camp.nextstep.edu.github.model
 
+import camp.nextstep.edu.github.domain.model.GithubRepositoryInfo
+
 data class GithubRepoUiModel(
     val fullName: String,
     val description: String,
 )
+
+fun GithubRepositoryInfo.toUiModel() {
+    GithubRepoUiModel(
+        fullName = this.fullName ?: "",
+        description = this.description ?: "",
+    )
+}
--- app/src/main/java/camp/nextstep/edu/github/ui/MainViewModel.kt
+++ app/src/main/java/camp/nextstep/edu/github/ui/MainViewModel.kt
@@ -3,6 +3,7 @@ package camp.nextstep.edu.github.ui
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import camp.nextstep.edu.github.domain.usecase.GetGithubRepositoriesUseCase
+import camp.nextstep.edu.github.model.toUiModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -13,7 +14,7 @@ class MainViewModel @Inject constructor(
 ): ViewModel() {
     fun getRepositories() {
         viewModelScope.launch {
-            getGithubRepositoriesUseCase.invoke()
+            getGithubRepositoriesUseCase.invoke().map { it.toUiModel() }
         }
     }
 }
