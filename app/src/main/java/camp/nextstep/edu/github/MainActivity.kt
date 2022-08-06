b8f1c5d11b29e5779c7c4f6ddbcab3f16bd5c01c
greedy0110 <greedy0110@gmail.com>
2022-08-06 22:19:22 +0900

    MainActivity 화면 코드 개발

--- app/src/main/java/camp/nextstep/edu/github/MainActivity.kt
+++ app/src/main/java/camp/nextstep/edu/github/MainActivity.kt
@@ -1,11 +1,71 @@
 package camp.nextstep.edu.github
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import camp.nextstep.edu.github.GitRepoAdapter.GitRepoViewHolder
+import camp.nextstep.edu.github.data.GithubRepository
+import camp.nextstep.edu.github.data.GithubService
+import camp.nextstep.edu.github.databinding.ActivityMainBinding
+import camp.nextstep.edu.github.databinding.ItemRepoBinding
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel: MainViewModel by viewModels()
+    private val gitRepoAdapter by lazy { GitRepoAdapter() }
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        initList()
     }
+    private fun initList() {
+        with(binding.list) {
+            itemAnimator = null
+            adapter = gitRepoAdapter
+        }
+        viewModel.repos.observe(this) {
+            gitRepoAdapter.submitList(it)
+        }
+    }
+}
+
+class GitRepoAdapter : ListAdapter<GithubRepository, GitRepoViewHolder>(diffCallback) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GitRepoViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_repo, parent, false)
+        return GitRepoViewHolder(view)
+    }
+    override fun onBindViewHolder(holder: GitRepoViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+    companion object {
+        private val diffCallback = object : DiffUtil.ItemCallback<GithubRepository>() {
+            override fun areItemsTheSame(
+                old: GithubRepository,
+                new: GithubRepository
+            ): Boolean = old == new
+
+            override fun areContentsTheSame(
+                old: GithubRepository,
+                new: GithubRepository
+            ): Boolean = old == new
+        }
+    }
+    class GitRepoViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val binding = ItemRepoBinding.bind(view)
+        fun bind(item: GithubRepository) { binding.item = item }
+    }
+}
+
+class MainViewModel : ViewModel() {
+    private val githubService: GithubService = TODO()
+    val repos: LiveData<List<GithubRepository>> = liveData { emit(githubService.getRepositories()) }
 }
