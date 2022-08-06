cb8bcc98d5df523958fc1dec6cd0cd22fe95ee66
Suhyeon Kim <wisemuji@gmail.com>
2022-08-06 23:26:21 +0900

    build: data 모듈에 Retrofit, 테스트 의존성 추가

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("kotlin")
+    id("kotlin-kapt")
 }
 
 java {
@@ -14,8 +15,18 @@ repositories {
 dependencies {
     implementation(project(":domain"))
 
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation("com.squareup.retrofit2:converter-moshi:$retrofitVersion")
+    implementation("com.squareup.okhttp3:okhttp:$okhttpVersion")
+    implementation("com.squareup.moshi:moshi-kotlin:$moshiVersion")
+    implementation("com.squareup.moshi:moshi-kotlin-codegen:$moshiVersion")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+
+    kapt("com.squareup.moshi:moshi-kotlin-codegen:$moshiVersion")
+    testImplementation("com.squareup.okhttp3:mockwebserver:$okhttpVersion")
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
     testImplementation("com.google.truth:truth:1.1.3")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
 }
 
 tasks.withType<Test> {
