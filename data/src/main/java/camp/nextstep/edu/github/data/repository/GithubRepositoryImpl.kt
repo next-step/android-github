030002a1080dc8e5c85f7c052c6f29f0b36345f9
Keich.Park <svclaw2000@gmail.com>
2022-08-06 22:59:51 +0900

    feat: baseUrl 주입

--- data/src/main/java/camp/nextstep/edu/github/data/repository/GithubRepositoryImpl.kt
+++ data/src/main/java/camp/nextstep/edu/github/data/repository/GithubRepositoryImpl.kt
@@ -1,15 +1,13 @@
 package camp.nextstep.edu.github.data.repository
 
 import camp.nextstep.edu.github.data.mapper.toDomain
+import camp.nextstep.edu.github.data.retrofit.GithubRetrofit
 import camp.nextstep.edu.github.data.retrofit.GithubService
-import camp.nextstep.edu.github.data.retrofit.RetrofitModule
 import camp.nextstep.edu.github.domain.model.Repository
 import camp.nextstep.edu.github.domain.repository.GithubRepository
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 
 internal class GithubRepositoryImpl(
-    private val githubService: GithubService = RetrofitModule.githubService
+    private val githubService: GithubService = GithubRetrofit().githubService
 ) : GithubRepository {
 
     override suspend fun fetchRepositoryList(): Result<List<Repository>> {
--- data/src/main/java/camp/nextstep/edu/github/data/retrofit/GithubRetrofit.kt
+++ data/src/main/java/camp/nextstep/edu/github/data/retrofit/GithubRetrofit.kt
@@ -4,12 +4,12 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
 
-object RetrofitModule {
-
-    private const val BASE_URL = "https://api.github.com"
+internal class GithubRetrofit(
+    baseUrl: String = "https://api.github.com"
+) {
 
     private val retrofit = Retrofit.Builder()
-        .baseUrl(BASE_URL)
+        .baseUrl(baseUrl)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
