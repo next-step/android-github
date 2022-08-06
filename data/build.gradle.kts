76c7bb8b7ce62a8c1ecc7e90c514497f4d07c93f
greedy0110 <greedy0110@gmail.com>
2022-08-06 22:55:50 +0900

    디버그를 위한 코드 추가

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -8,6 +8,9 @@ repositories {
 }
 
 dependencies {
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
     implementation("com.squareup.moshi:moshi:1.13.0")
