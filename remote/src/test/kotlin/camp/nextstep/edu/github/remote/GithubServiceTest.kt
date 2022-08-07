e37c75a3a5a66db98c4dc0fe14aecd8ebb01fe95
greedy0110 <greedy0110@gmail.com>
2022-08-07 23:39:09 +0900

    BeforeEach에 테스트 초기화

--- remote/src/test/kotlin/camp/nextstep/edu/github/remote/GithubServiceTest.kt
+++ remote/src/test/kotlin/camp/nextstep/edu/github/remote/GithubServiceTest.kt
@@ -1,11 +1,14 @@
 package camp.nextstep.edu.github.remote
 
 import camp.nextstep.edu.github.data.DataException
+import camp.nextstep.edu.github.data.GitRepoRepository
 import com.google.common.truth.Truth.assertThat
 import kotlinx.coroutines.runBlocking
 import okhttp3.HttpUrl
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import retrofit2.Retrofit
@@ -15,16 +18,23 @@ import java.io.File
 
 internal class GithubServiceTest {
 
+    private lateinit var server: MockWebServer
+    private lateinit var service: GitRepoRepository
+
+    @BeforeEach
+    fun setup() {
+        server = MockWebServer()
+        server.start()
+        val retrofit = createRetrofit(server.url("/"))
+        service = createGitRepoRepository(retrofit)
+    }
+
     @Test
     fun `Repository를 받아온다`() = runBlocking {
         // given
-        val server = MockWebServer()
         // TODO sinseungmin 2022/08/04: 서버 구현체를 너무 많이 알고있는 코드 아님?
         val mockJsonString = getMockGetRepoJsonString()
         server.enqueue(MockResponse().setBody(mockJsonString))
-        server.start()
-        val retrofit = createRetrofit(server.url("/"))
-        val service = createGitRepoRepository(retrofit)
 
         // when
         val actual = service.getGitRepos()
@@ -36,11 +46,7 @@ internal class GithubServiceTest {
     @Test
     fun `Repository를 받아온는데 실패한다`(): Unit = runBlocking {
         // given
-        val server = MockWebServer()
         server.enqueue(MockResponse().setHttp2ErrorCode(500))
-        server.start()
-        val retrofit = createRetrofit(server.url("/"))
-        val service = createGitRepoRepository(retrofit)
 
         // when, then
         assertThrows<DataException> { service.getGitRepos() }
