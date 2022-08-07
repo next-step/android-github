b00f67d7441ef89e2e931dc8b9495a0fcc1bcfd9
Keich.Park <svclaw2000@gmail.com>
2022-08-07 19:33:29 +0900

    build: fragment lifecycle 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+    implementation("androidx.fragment:fragment-ktx:1.5.1")
 
     kapt(HiltKapt)
     implementation(HiltAndroid)
