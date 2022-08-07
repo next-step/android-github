da24635d46c5fac69c3c0fca41cff06cdedc9f73
1stpie <1stpie@naver.com>
2022-08-08 00:36:20 +0900

    refactor: interface mapper 활용

--- data/src/main/java/camp/nextstep/edu/github/data/DefaultGitHubRepository.kt
+++ data/src/main/java/camp/nextstep/edu/github/data/DefaultGitHubRepository.kt
@@ -1,9 +1,12 @@
 package camp.nextstep.edu.github.data
 
 import camp.nextstep.edu.github.domain.GithubRepository
+import camp.nextstep.edu.github.domain.Repositories
 
 internal class DefaultGitHubRepository(
     private val gitHubService: GitHubService
 ) : GithubRepository {
-    override suspend fun loadRepositories() = gitHubService.listRepos().let(RepositoriesMapper::mapToDomain)
+    override suspend fun loadRepositories(): Repositories = gitHubService.listRepos()
+        .map { it.toDomain() }
+        .let { Repositories(it) }
 }
--- data/src/main/java/camp/nextstep/edu/github/data/RepoModel.kt
+++ data/src/main/java/camp/nextstep/edu/github/data/RepoModel.kt
@@ -1,11 +1,14 @@
 package camp.nextstep.edu.github.data
 
+import camp.nextstep.edu.github.domain.Repo
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 internal data class RepoModel(
-    @Json(name="id") val id: Int,
-    @Json(name="full_name") val fullName: String,
-    @Json(name="description") val description: String?
-)
+    @Json(name = "id") val id: Int,
+    @Json(name = "full_name") val fullName: String,
+    @Json(name = "description") val description: String?
+) : DataToDomainMapper<Repo> {
+    override fun toDomain(): Repo = Repo(id, fullName, description)
+}
