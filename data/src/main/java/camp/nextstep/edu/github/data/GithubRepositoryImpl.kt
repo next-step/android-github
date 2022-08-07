ca15c8682b8ae115809b93ef31a4532cc09f428d
link-js <link.js@kakaocorp.com>
2022-08-07 23:29:12 +0900

    refactor : list 일급 컬렉션으로 변경

--- data/src/main/java/camp/nextstep/edu/github/data/GithubRepositoryImpl.kt
+++ data/src/main/java/camp/nextstep/edu/github/data/GithubRepositoryImpl.kt
@@ -1,13 +1,13 @@
 package camp.nextstep.edu.github.data
 
 import camp.nextstep.edu.github.domain.GithubRepository
-import camp.nextstep.edu.github.domain.Repository
+import camp.nextstep.edu.github.domain.Repositories
 
 /**
  * Created by link.js on 2022. 08. 05..
  */
 internal class GithubRepositoryImpl : GithubRepository {
-    override suspend fun getRepositories(): List<Repository> {
-        return GithubServiceFactory.githubApi.getRepositories().map { it.toDomain() }
+    override suspend fun getRepositories(): Repositories {
+        return Repositories(GithubServiceFactory.githubApi.getRepositories().map { it.toDomain() })
     }
 }
---
+++ domain/src/main/java/camp/nextstep/edu/github/domain/Repositories.kt
@@ -0,0 +1,8 @@
+package camp.nextstep.edu.github.domain
+
+/**
+ * Created by link.js on 2022. 08. 07..
+ */
+data class Repositories(val list: List<Repository> = emptyList()) {
+
+}
