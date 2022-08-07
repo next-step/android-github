87c3b165d37008eb250967b9141e71a8a7247d57
greedy0110 <greedy0110@gmail.com>
2022-08-07 09:36:44 +0900

    data module에 필요없는 의존성 제거

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("kotlin")
-    id("kotlin-kapt")
 }
 
 repositories {
@@ -10,18 +9,7 @@ repositories {
 dependencies {
     implementation(project(":domain"))
 
-    implementation("com.squareup.okhttp3:okhttp:4.10.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
-
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
-    implementation("com.squareup.moshi:moshi:1.13.0")
-    kapt("com.squareup.moshi:moshi-kotlin-codegen:1.13.0")
-
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
-
-    testImplementation("com.squareup.okhttp3:mockwebserver:4.10.0")
-
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("com.google.truth:truth:1.1.3")
     testImplementation("io.mockk:mockk:1.12.3")
