a298e5ff24bbc45371c0cdd97d9d39eb14dfad9f
Keich.Park <svclaw2000@gmail.com>
2022-08-08 20:13:29 +0900

    feat: 바인딩 어댑터의 `custom:` 제거

--- app/src/main/java/camp/nextstep/edu/github/binding_adapter/RecyclerViewBindingAdapter.kt
+++ app/src/main/java/camp/nextstep/edu/github/binding_adapter/RecyclerViewBindingAdapter.kt
@@ -5,7 +5,7 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 
 @Suppress("UNCHECKED_CAST")
-@BindingAdapter("custom:submitList")
+@BindingAdapter("submitList")
 fun <T, VH : RecyclerView.ViewHolder> submitList(recyclerView: RecyclerView, itemList: List<T>) {
     (recyclerView.adapter as? ListAdapter<T, VH>)?.submitList(itemList)
 }
