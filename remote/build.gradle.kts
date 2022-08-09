d54474243f00b30af688331c644b6a835430065b
greedy0110 <greedy0110@gmail.com>
2022-08-09 20:50:34 +0900

    코드 정리

--- remote/build.gradle.kts
+++ remote/build.gradle.kts
@@ -10,17 +10,17 @@ repositories {
 dependencies {
     implementation(project(":data"))
 
-    api("com.squareup.okhttp3:okhttp:4.10.0")
-    api("com.squareup.okhttp3:logging-interceptor:4.10.0")
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
 
-    api("com.squareup.retrofit2:retrofit:2.9.0")
-    api("com.squareup.retrofit2:converter-moshi:2.9.0")
-    api("com.squareup.moshi:moshi:1.13.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+    implementation("com.squareup.moshi:moshi:1.13.0")
     kapt("com.squareup.moshi:moshi-kotlin-codegen:1.13.0")
 
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
 
-    testApi("com.squareup.okhttp3:mockwebserver:4.10.0")
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.10.0")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("com.google.truth:truth:1.1.3")
