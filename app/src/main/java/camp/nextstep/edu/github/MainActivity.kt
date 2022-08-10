6ea9049dfcf279c38c9d4bbefbdfa54ebe41619f
ethan <noahkim.j@gmail.com>
2022-08-10 22:18:51 +0900

    사용이 안되는 import 제거, 자동 정렬

--- app/src/main/java/camp/nextstep/edu/github/MainActivity.kt
+++ app/src/main/java/camp/nextstep/edu/github/MainActivity.kt
@@ -2,9 +2,6 @@ package camp.nextstep.edu.github
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import camp.nextstep.edu.github.data.DataInjector
-import camp.nextstep.edu.github.domain.Repo
-
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- data/src/main/java/camp/nextstep/edu/github/data/DataInjector.kt
+++ data/src/main/java/camp/nextstep/edu/github/data/DataInjector.kt
@@ -5,13 +5,14 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object DataInjector {
-    fun provideGithubRepository() : GithubRepository {
+    fun provideGithubRepository(): GithubRepository {
         return GithubRepositoryImpl(provideGithubService())
     }
 
-    private fun provideGithubService(baseUrl: String = "https://api.github.com/"): GithubService = Retrofit.Builder()
-        .baseUrl(baseUrl)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-        .create(GithubService::class.java)
+    private fun provideGithubService(baseUrl: String = "https://api.github.com/"): GithubService =
+        Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(GithubService::class.java)
 }
