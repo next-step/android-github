1fa484f05a8ad827bd26939070b35309373edad9
ethan <noahkim.j@gmail.com>
2022-08-10 23:12:24 +0900

    description필드에 대해서도 같이 검증
    isSameInstanceAs - 주소값 비교
    isEqualTo - 객체 내용 비교
    isInstanceOf - 객체 타입비교

--- data/src/test/java/camp/nextstep/edu/github/data/GithubRepositoryImplTest.kt
+++ data/src/test/java/camp/nextstep/edu/github/data/GithubRepositoryImplTest.kt
@@ -1,12 +1,14 @@
 package camp.nextstep.edu.github.data
 
 import camp.nextstep.edu.github.domain.GithubRepository
+import camp.nextstep.edu.github.domain.Repo
 import com.google.common.truth.Truth.assertThat
 import kotlinx.coroutines.runBlocking
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+
 import java.io.File
 
 
@@ -21,15 +23,19 @@ class GithubRepositoryImplTest {
     }
 
     @Test
-    fun `Github Repository 로드한다`() = runBlocking {
+    fun `Github Repository 로드하여 첫번째 Repo 객체를 검증한다`() = runBlocking {
         val response = MockResponse()
             .setBody(File("src/test/resources/repositories.json").readText())
             .setResponseCode(200)
 
         server.enqueue(response)
 
-        val expected = "mojombo/grit"
-        val actual = repository.getRepositories().firstOrNull()?.fullName
+        val expected = Repo(
+            "mojombo/grit",
+            "**Grit is no longer maintained. Check out libgit2/rugged.** Grit gives you object oriented read/write access to Git repositories via Ruby."
+        )
+
+        val actual = repository.getRepositories().firstOrNull()
 
         assertThat(actual).isEqualTo(expected)
     }
