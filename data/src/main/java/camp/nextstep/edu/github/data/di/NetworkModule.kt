c4fd009b4ee6c96ce43c545eb75f955919316149
Ben <ben.to@kakaocorp.com>
2022-08-11 23:32:33 +0900

    Step3(feat): 빠져있는 의존성 추가

--- data/src/main/java/camp/nextstep/edu/github/data/di/NetworkModule.kt
+++ data/src/main/java/camp/nextstep/edu/github/data/di/NetworkModule.kt
@@ -2,18 +2,29 @@ package camp.nextstep.edu.github.data.di
 
 import camp.nextstep.edu.github.data.service.GitHubService
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
 
-@InstallIn(SingletonComponent::class)
 @Module
-internal object NetworkModule {
-    fun provideGitHubService(): GitHubService =
+@InstallIn(SingletonComponent::class)
+internal class NetworkModule {
+    @Provides
+    @Singleton
+    fun provideGitHubService(okHttpClient: OkHttpClient): GitHubService =
         Retrofit.Builder()
             .baseUrl(GitHubService.BASE_URL)
+            .client(okHttpClient)
             .addConverterFactory(MoshiConverterFactory.create())
             .build()
             .create(GitHubService::class.java)
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient =
+        OkHttpClient.Builder().build()
 }
--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,6 +1,8 @@
+import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
+
 plugins {
-    id ("java-library")
     id ("kotlin")
+    kotlin ("kapt")
 }
 
 java {
@@ -10,4 +12,5 @@ java {
 
 dependencies {
     implementation("com.google.dagger:hilt-core:2.42")
+    kapt("com.google.dagger:hilt-android-compiler:2.42")
 }
