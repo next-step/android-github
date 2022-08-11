c3aa9d7c4b6a4c7707857a3c271b8c10a9b62883
Ben <ben.to@kakaocorp.com>
2022-08-11 23:31:59 +0900

    Step3(feat) : MainActivity, DataBinding

--- app/src/main/java/camp/nextstep/edu/github/MainActivity.kt
+++ app/src/main/java/camp/nextstep/edu/github/MainActivity.kt
@@ -1,13 +1,41 @@
 package camp.nextstep.edu.github
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import camp.nextstep.edu.github.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by viewModels()
+    private val adapter: GitHubReposAdapter by lazy { GitHubReposAdapter() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initRecyclerView()
+        loadGitHubRepos()
+        observeViewModel()
+    }
+
+    private fun initRecyclerView() {
+        binding.recyclerView.adapter = adapter
+    }
+
+    private fun loadGitHubRepos() {
+        lifecycleScope.launchWhenStarted {
+            viewModel.loadGitHubRepos()
+        }
+    }
+
+    private fun observeViewModel() {
+        viewModel.gitHubRepos.observe(this) {
+            adapter.submitList(it.toViewDataList())
+        }
     }
 }
