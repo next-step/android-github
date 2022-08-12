bfca3aa67e626a5b4484d3022809b5b871f881a3
Choiyounho <maxosa78@gmail.com>
2022-08-13 01:27:39 +0900

    refactor : 테스트 코드 피드백 적용

--- data/src/test/java/com/nextstep/edu/github/NetworkTest.kt
+++ data/src/test/java/com/nextstep/edu/github/NetworkTest.kt
@@ -4,7 +4,9 @@ import camp.nextstep.edu.github.data.GithubApi
 import camp.nextstep.edu.github.data.GithubRepositoryResponse
 import camp.nextstep.edu.github.domain.GithubRepository
 import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.jupiter.api.BeforeEach
@@ -13,6 +15,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import java.io.File
 
+@OptIn(ExperimentalCoroutinesApi::class)
 internal class NetworkTest {
 
     private lateinit var mockWebServer: MockWebServer
@@ -29,7 +32,21 @@ internal class NetworkTest {
     }
 
     @Test
-    fun `깃헙 저장소 목록을 받아온다`() = runBlocking {
+    fun `깃헙 저장소를 불러오는데 빈 값이 불러와질 때 테스트`() = runTest {
+        // given
+        val response = MockResponse().setBody(File("src/test/resources/empty.json").readText())
+        mockWebServer.enqueue(response)
+
+        // when
+        val actual = githubApi.getGithubRepositories()
+
+        // then
+        val expected = listOf<GithubRepositoryResponse>()
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `깃헙 저장소 목록을 받아온다`() = runTest {
         // given
         val response = MockResponse().setBody(File("src/test/resources/response.json").readText())
         mockWebServer.enqueue(response)
@@ -46,4 +63,5 @@ internal class NetworkTest {
         )
         assertThat(actual).isEqualTo(expected)
     }
+
 }
