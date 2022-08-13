cb94de3dc42c5247137659f8bea20eb1147f8c39
hyowan <woga1999@gmail.com>
2022-08-14 02:57:53 +0900

    feat: 네트워크 모듈 수정

--- data/src/main/java/camp/nextstep/edu/github/data/NetworkModule.kt
+++ data/src/main/java/camp/nextstep/edu/github/data/NetworkModule.kt
@@ -1,28 +1,46 @@
 package camp.nextstep.edu.github.data
 
-import camp.nextstep.edu.github.domain.GithubRepository
-import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
+import okhttp3.logging.HttpLoggingInterceptor
 
 @Module
 @InstallIn(SingletonComponent::class)
 class NetworkModule {
 
-    private val BASE_URL = "https://api.github.com/"
+    private val BASE_URL = "https://api.github.com"
 
     @Provides
     @Singleton
-    internal fun provideApiService(): ApiService {
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(
+                HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BODY
+                }
+            )
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
         return Retrofit.Builder()
+            .client(okHttpClient)
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-            .create(ApiService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    internal fun provideGithubService(retrofit: Retrofit): ApiService {
+        return retrofit.create(ApiService::class.java)
     }
 }
