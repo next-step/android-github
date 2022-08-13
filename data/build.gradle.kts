a3e37f7a2afa441319776b148b5f973d038b3a95
Sangmeebee <apfhdznzl@gmail.com>
2022-08-14 01:29:42 +0900

    feat: data 모듈 retrofit, converter, okhttp 관련 의존성 추가 관려이슈: step2-github(http)

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -9,4 +9,13 @@ java {
 
 dependencies {
     implementation(project(":domain"))
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.google.code.gson:gson:2.9.1")
+    // define a BOM and its version
+    implementation(platform("com.squareup.okhttp3:okhttp-bom:4.10.0"))
+    // define any required OkHttp artifacts without version
+    implementation("com.squareup.okhttp3:okhttp")
+    implementation("com.squareup.okhttp3:logging-interceptor")
 }
