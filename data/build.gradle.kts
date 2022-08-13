f36d985dd77307eec6ff1080bd240a4fc4c91d56
Sangmeebee <apfhdznzl@gmail.com>
2022-08-14 03:05:22 +0900

    feat: data 모듈에 MockWebServer를 이용하여 github network test code 작성 관련이슈: step2-github(http)

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -14,8 +14,20 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.google.code.gson:gson:2.9.1")
     // define a BOM and its version
-    implementation(platform("com.squareup.okhttp3:okhttp-bom:4.10.0"))
+    implementation(platform("com.squareup.okhttp3:okhttp-bom:$okhttpVersion"))
     // define any required OkHttp artifacts without version
     implementation("com.squareup.okhttp3:okhttp")
     implementation("com.squareup.okhttp3:logging-interceptor")
+
+    testImplementation("com.squareup.okhttp3:mockwebserver:$okhttpVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
+    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.9.0")  // junit4 지원
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("com.google.truth:truth:1.1.3")
+    testImplementation("io.mockk:mockk:1.12.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
