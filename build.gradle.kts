4eb5980ae35e9454c098c886c2082c0ef020ab64
1stpie <1stpie@naver.com>
2022-08-13 15:38:21 +0900

    chore: hilt 를 classpath 에서 id 로 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,11 @@ plugins {
     id("com.android.application") version Versions.gradleVersion apply false
     id("com.android.library") version Versions.gradleVersion apply false
     id("org.jetbrains.kotlin.android") version Versions.kotlinVersion apply false
+    id("com.google.dagger.hilt.android") version Versions.Hilt.hiltVersion apply false
 }
 
 buildscript {
     dependencies {
-        classpath("com.google.dagger:hilt-android-gradle-plugin:${Versions.Hilt.hiltVersion}")
         classpath(kotlin("gradle-plugin", version = Versions.kotlinVersion))
         classpath(kotlin("serialization", version = Versions.kotlinVersion))
     }
