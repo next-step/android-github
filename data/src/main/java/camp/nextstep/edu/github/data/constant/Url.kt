79c4a691ea1bbbf4a410ec8cdeb276cc17ea46d6
Sangmeebee <apfhdznzl@gmail.com>
2022-08-15 04:06:05 +0900

    refacor: Url 상수 값을 한곳에서 정의해주는 방식에서 사용하는 곳에서 개별적으로 정의하게 수정 관련이슈: step2-github(http)

--- data/src/main/java/camp/nextstep/edu/github/data/constant/Url.kt
+++
@@ -1,3 +0,0 @@
-package camp.nextstep.edu.github.data.constant
-
-const val GITHUB_BASE_URL = "https://api.github.com"
--- data/src/main/java/camp/nextstep/edu/github/data/service/APIClient.kt
+++ data/src/main/java/camp/nextstep/edu/github/data/service/APIClient.kt
@@ -1,12 +1,13 @@
 package camp.nextstep.edu.github.data.service
 
-import camp.nextstep.edu.github.data.constant.GITHUB_BASE_URL
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 internal object APIClient {
+
+    private const val GITHUB_BASE_URL = "https://api.github.com"
 
     val retrofit = Retrofit.Builder()
         .client(getOkHttpClient())
