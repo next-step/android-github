2e966b996b7da5e442bbc6e84b75e2a29b7e7a69
KimKwangYoung <kwangyoung.dev@gmail.com>
2022-08-16 00:08:54 +0900

    UI 구현
    - Hilt 추가
    - MVVM 패턴 적용

--- app/src/main/java/camp/nextstep/edu/github/MainActivity.kt
+++ app/src/main/java/camp/nextstep/edu/github/MainActivity.kt
@@ -2,10 +2,33 @@ package camp.nextstep.edu.github
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
+import camp.nextstep.edu.github.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel:MainViewModel by viewModels()
+
+    private val repoAdapter by lazy { GithubRepoListAdapter() }
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.lifecycleOwner = this
+
+        binding.recyclerView.adapter = repoAdapter
+        observeData()
+    }
+
+    private fun observeData() {
+        viewModel.gitRepo.observe(this) {
+            repoAdapter.submitList(it.list())
+        }
     }
 }
