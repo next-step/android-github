6304f62fbac67705789f1efab2bf33ad00a0b471
이연재 <yeonberry22@gmail.com>
2022-08-15 18:26:09 +0900

    feat: step2 코드 리뷰 반영

--- data/src/main/java/camp/nextstep/edu/github/data/GitHubRepositoryImpl.kt
+++ data/src/main/java/camp/nextstep/edu/github/data/GitHubRepositoryImpl.kt
@@ -4,10 +4,15 @@ import camp.nextstep.edu.github.domain.GitHubRepository
 import camp.nextstep.edu.github.domain.GitHubRepositoryItem
 
 internal class GitHubRepositoryImpl(private val gitHubService: GitHubService) : GitHubRepository {
-    override suspend fun getGitHubRepositoryList(): Result<List<GitHubRepositoryItem>> =
-        runCatching {
-            gitHubService.getGitHubRepositoryList().body()?.map {
-                it.toGitHubRepositoryItem()
-            } ?: throw NullPointerException()
+    override suspend fun getGitHubRepositoryList(): Result<List<GitHubRepositoryItem>> {
+        val response = gitHubService.getGitHubRepositoryList()
+        if (response.isSuccessful) {
+            val gitHubRepositoryList =
+                response.body()?.map { it.toGitHubRepositoryItem() } ?: return Result.failure(
+                    NullPointerException()
+                )
+            return Result.success(gitHubRepositoryList)
         }
+        return Result.failure(Exception())
+    }
 }
