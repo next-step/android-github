175d172b608c0a880bb98d85a24d942ec2a67b64
ChoiYounho <maxosa78@gmail.com>
2022-08-16 22:22:08 +0900

    refactor : UiState 리팩토링

--- app/src/main/java/camp/nextstep/edu/github/UiState.kt
+++ app/src/main/java/camp/nextstep/edu/github/UiState.kt
@@ -3,7 +3,7 @@ package camp.nextstep.edu.github
 sealed class UiState<out T> {
 
     object Loading : UiState<Nothing>()
-    data class Success<T>(val data: T?) : UiState<T>()
+    data class Success<T>(val data: T) : UiState<T>()
     data class Error<T>(val throwable: Throwable?) : UiState<T>()
 
 }
