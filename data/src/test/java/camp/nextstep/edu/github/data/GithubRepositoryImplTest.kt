b498ebe079b9d720fe499fb67b2fb9c2ad4c8cb6
KimKwangYoung <kwangyoung.dev@gmail.com>
2022-08-18 00:22:38 +0900

    코드 정리

--- data/src/test/java/camp/nextstep/edu/github/data/GithubRepositoryImplTest.kt
+++ data/src/test/java/camp/nextstep/edu/github/data/GithubRepositoryImplTest.kt
@@ -1,7 +1,5 @@
 package camp.nextstep.edu.github.data
 
-import camp.nextstep.edu.github.domain.GithubRepo
-import camp.nextstep.edu.github.domain.GithubRepositories
 import camp.nextstep.edu.github.domain.GithubRepository
 import com.google.common.truth.Truth.assertThat
 import io.mockk.coEvery
@@ -39,18 +37,16 @@ internal class GithubRepositoryImplTest {
     @Test
     fun 저장소를_불러오는_메소드를_호출했을_때_반환되는_값을_확인한다() = runTest {
         //given
-        val expected =  GithubRepositories(
-            listOf(
-                GithubRepo(-1, "test", "test description")
-            )
-        )
-        coEvery { githubApiService.repositories() } returns listOf(GithubRepoResponse(-1, "test", "test description"))
+        val response = listOf<GithubRepoResponse>(GithubRepoResponse(-1, "test", "test description"))
+        coEvery { githubApiService.repositories() } returns response
+        val expected = response.map(GithubRepoResponse::toDomain)
 
         //when
         val actual = githubRepository.getRepo()
 
+
         //then
         coVerify { githubApiService.repositories() }
-        assertThat(actual.list).isEqualTo(expected.list)
+        assertThat(actual.list).isEqualTo(expected)
     }
 }
