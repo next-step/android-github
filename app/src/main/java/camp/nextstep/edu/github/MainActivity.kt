ae5853f0405df890d4d73e241c104a9a5d9bfbb4
Sangmeebee <apfhdznzl@gmail.com>
2022-08-19 04:34:07 +0900

    feat: MainViewModel 구현 & MainActivity에서 observe해서 View관련 코드 구현 관련이슈: step3-github(ui)

--- app/src/main/java/camp/nextstep/edu/github/MainActivity.kt
+++ app/src/main/java/camp/nextstep/edu/github/MainActivity.kt
@@ -1,10 +1,17 @@
 package camp.nextstep.edu.github
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import camp.nextstep.edu.github.databinding.ActivityMainBinding
+import camp.nextstep.edu.github.domain.model.GithubStorage
+import camp.nextstep.edu.github.domain.model.NetworkState
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -19,14 +26,57 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply {
+            lifecycleOwner = this@MainActivity
+            mainVM = this@MainActivity.mainVM
+        }
         setContentView(binding.root)
 
         initRecyclerView()
+        observeUIState()
+        observeErrorEvent()
+        observeGithubStorageEvent()
     }
 
     private fun initRecyclerView() {
         binding.recyclerview.adapter = githubStorageAdapter
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    private fun observeUIState() = lifecycleScope.launch {
+        repeatOnLifecycle(Lifecycle.State.STARTED) {
+            mainVM.uiState.collect { uiState ->
+                when (uiState) {
+                    is NetworkState.Loading -> {
+                        mainVM.updateLoadingEvent(true)
+                    }
+                    is NetworkState.Success<*> -> {
+                        mainVM.updateLoadingEvent(false)
+                        mainVM.updateGithubStorage(uiState.data as List<GithubStorage>)
+                    }
+                    is NetworkState.Error -> {
+                        mainVM.updateLoadingEvent(false)
+                        mainVM.updateErrorEvent(uiState.throwable)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun observeErrorEvent() = lifecycleScope.launch {
+        repeatOnLifecycle(Lifecycle.State.STARTED) {
+            mainVM.errorEvent.collect { networkState ->
+                Toast.makeText(this@MainActivity, networkState.message, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun observeGithubStorageEvent() = lifecycleScope.launch {
+        repeatOnLifecycle(Lifecycle.State.STARTED) {
+            mainVM.githubStorages.collect { githubStorages ->
+                githubStorageAdapter.submitList(githubStorages)
+            }
+        }
     }
 
 }
--- domain/src/main/java/camp/nextstep/edu/github/domain/model/NetworkState.kt
+++ domain/src/main/java/camp/nextstep/edu/github/domain/model/NetworkState.kt
@@ -3,5 +3,5 @@ package camp.nextstep.edu.github.domain.model
 sealed class NetworkState {
     object Loading : NetworkState()
     class Success<T>(val data: T) : NetworkState()
-    class Error(val throwable: Throwable?) : NetworkState()
+    class Error(val throwable: Throwable) : NetworkState()
 }
