d15ad53210254299df6384ccf7763f6f80217808
Sangmeebee <apfhdznzl@gmail.com>
2022-08-23 01:53:57 +0900

    fix: 불필요한 databinding 코드 제거 관련이슈: step3-github(ui)

--- app/src/main/java/camp/nextstep/edu/github/MainActivity.kt
+++ app/src/main/java/camp/nextstep/edu/github/MainActivity.kt
@@ -24,10 +24,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater).apply {
-            lifecycleOwner = this@MainActivity
-            mainViewModel = this@MainActivity.mainViewModel
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         initRecyclerView()
