c76a939a7084942a3a19ec57b4ad92eac16783bf
Sangmeebee <apfhdznzl@gmail.com>
2022-08-23 01:52:12 +0900

    refactor: Activity class와 xml 두 곳에서 하나의 데이터를 observe하는 것을 Activity class에서만 observe 하게 수정 관련이슈: step3-github(ui)

--- app/src/main/java/camp/nextstep/edu/github/MainActivity.kt
+++ app/src/main/java/camp/nextstep/edu/github/MainActivity.kt
@@ -1,6 +1,7 @@
 package camp.nextstep.edu.github
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -42,16 +43,24 @@ class MainActivity : AppCompatActivity() {
         repeatOnLifecycle(Lifecycle.State.STARTED) {
             mainViewModel.uiState.collect { uiState ->
                 when (uiState) {
-                    is UIState.Loading -> {}
+                    is UIState.Loading -> {
+                        showLoading(true)
+                    }
                     is UIState.Success -> {
+                        showLoading(false)
                         githubStorageAdapter.submitList(uiState.data)
                     }
                     is UIState.Error -> {
+                        showLoading(false)
                         showToast(uiState.throwable.message)
                     }
                 }
             }
         }
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        binding.progressbar.visibility = if (isLoading) View.VISIBLE else View.GONE
     }
 
     private fun showToast(message: String?) {
