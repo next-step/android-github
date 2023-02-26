3c49fc5bc042ff04038aece988fd699a3c75f08d
jpoh281 <hjp4869@naver.com>
2023-02-27 00:05:03 +0900

    feat: data, domain 모듈 생성 및 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,9 +48,12 @@ dependencies {
     implementation("com.google.android.material:material:1.7.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.fragment:fragment-ktx:1.5.5")
+    implementation(project(":data"))
+    implementation(project(":domain"))
 
     testImplementation("junit:junit:4.13.2")
     testImplementation("com.google.truth:truth:1.1.3")
+
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
