f59161c5b64cd8c3c27a895008f3206e5fbb7824
JaeHwan <>
2023-03-02 13:01:49 +0900

    refactor: MainViewModelTest Code 수정

--- app/src/test/java/camp/nextstep/edu/github/MainViewModelTest.kt
+++ app/src/test/java/camp/nextstep/edu/github/MainViewModelTest.kt
@@ -1,14 +1,18 @@
 package camp.nextstep.edu.github
 
+import com.google.common.truth.Truth
+import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import dagger.hilt.android.testing.HiltTestApplication
 import io.mockk.*
 import kotlinx.coroutines.test.runTest
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
+import javax.inject.Inject
 
 
 @HiltAndroidTest
@@ -16,21 +20,28 @@ import org.robolectric.annotation.Config
 @Config(application = HiltTestApplication::class)
 class MainViewModelTest {
 
+    @get:Rule
+    val hiltRule = HiltAndroidRule(this)
+
     private lateinit var viewModel: MainViewModel
+
+    @set:Inject
+    lateinit var repository: GitHubRepository
 
     @Before
     fun setUp() {
-        viewModel = mockk()
+        hiltRule.inject()
+        viewModel = MainViewModel(repository)
     }
 
     @Test
     fun `깃허브 레포지토리 데이터 가져오기 테스트`() = runTest {
         // when
-        coEvery { viewModel.getGithubRepositoryInfo() } just runs
+        val actual = repository.getGitHubRepositoryInfo()
 
         viewModel.getGithubRepositoryInfo()
+        val expected = viewModel.githubRepositoryInfo.getOrAwaitValue()
 
-        // then
-        coVerify { viewModel.getGithubRepositoryInfo() }
+        Truth.assertThat(actual).isEqualTo(expected)
     }
 }
