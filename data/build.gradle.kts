d25256f80493ec6e54a70f72e19ec8ce0e63b0ce
hyejin <hyejin_one@naver.com>
2023-03-02 23:43:09 +0900

    feat: 1단계 - Github(모듈 분리) (#90)

---
+++ data/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("java-library")
+    id("org.jetbrains.kotlin.jvm")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+dependencies{
+    implementation(project(":domain"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:${Version.kotlin}")
+    // Junit
+    testImplementation("junit:junit:${Version.JUNIT4}")
+    testImplementation("com.google.truth:truth:${Version.TRUTH}")
+
+    // Mockk
+    testImplementation("io.mockk:mockk:${Version.MOCKK}")
+}
