c5f671fa2108f456ea662ab69695e7dfa506e928
jpoh281 <hjp4869@naver.com>
2023-03-04 01:18:01 +0900

    feat: app 모듈에 Hilt 적용

--- app/src/main/java/camp/nextstep/edu/github/MainActivity.kt
+++ app/src/main/java/camp/nextstep/edu/github/MainActivity.kt
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import camp.nextstep.edu.github.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
--- app/src/main/java/camp/nextstep/edu/github/RepositoryViewModel.kt
+++ app/src/main/java/camp/nextstep/edu/github/RepositoryViewModel.kt
@@ -4,15 +4,15 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.data.Injector
 import com.example.domain.model.RepositoryResource
 import com.example.domain.usecase.GetRepositoryUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class RepositoryViewModel(
-    private val getRepositoryUseCase: GetRepositoryUseCase = GetRepositoryUseCase(
-        Injector.providerGitHubRepository()
-    )
+@HiltViewModel
+class RepositoryViewModel @Inject constructor(
+    val getRepositoryUseCase: GetRepositoryUseCase
 ) : ViewModel() {
 
     private val _repositories = MutableLiveData<List<RepositoryResource>>(emptyList())
