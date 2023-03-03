f815848649fa8c6118cf3b67c53c5c09c20ab740
jpoh281 <hjp4869@naver.com>
2023-03-04 00:38:01 +0900

    feat: ViewModel 구현

--- app/src/main/java/camp/nextstep/edu/github/RepositoryViewModel.kt
+++ app/src/main/java/camp/nextstep/edu/github/RepositoryViewModel.kt
@@ -1,4 +1,27 @@
 package camp.nextstep.edu.github
 
-class RepositoryViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.data.Injector
+import com.example.domain.model.RepositoryResource
+import com.example.domain.usecase.GetRepositoryUseCase
+import kotlinx.coroutines.launch
+
+class RepositoryViewModel(
+    private val getRepositoryUseCase: GetRepositoryUseCase = GetRepositoryUseCase(
+        Injector.providerGitHubRepository()
+    )
+) : ViewModel() {
+
+    private val _repositories = MutableLiveData<List<RepositoryResource>>(emptyList())
+    val repositories: LiveData<List<RepositoryResource>>
+        get() = _repositories
+
+    init {
+        viewModelScope.launch {
+            _repositories.value = getRepositoryUseCase.invoke()
+        }
+    }
 }
