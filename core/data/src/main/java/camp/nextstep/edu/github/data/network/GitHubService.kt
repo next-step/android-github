8cf1e514cd306d72ab83595e8c1fb0b0b6fe1b9e
minhyukseul <mioyo2@gmail.com>
2023-08-27 10:41:54 +0900

    refactor: 1단계 미션부분까지 적용

--- core/data/src/main/java/camp/nextstep/edu/github/data/network/GitHubService.kt
+++
@@ -1,9 +0,0 @@
-package camp.nextstep.edu.github.data.network
-
-import camp.nextstep.edu.github.domain.GithubRepository
-import retrofit2.http.GET
-
-interface GitHubService {
-    @GET("/repositories")
-    suspend fun getRepositories(): List<GithubRepository>
-}
--- core/domain/src/main/java/camp/nextstep/edu/github/domain/network/GitHubDataSource.kt
+++
@@ -1,7 +0,0 @@
-package camp.nextstep.edu.github.domain.network
-
-import camp.nextstep.edu.github.domain.GithubRepository
-
-interface GitHubDataSource {
-    suspend fun fetchRepositories(): List<GithubRepository>
-}
