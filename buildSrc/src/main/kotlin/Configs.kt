68d97098752962df5625d5f51ec6d59877ff6ff9
duru.hi <hwanggyuil@gmail.com>
2023-08-30 23:12:23 +0900

    feat. data 모듈 라이브러리 추가

--- buildSrc/src/main/kotlin/Configs.kt
+++ buildSrc/src/main/kotlin/Configs.kt
@@ -4,4 +4,11 @@ object Version {
     const val compileSdk = 33
     const val minSdk = 26
     const val targetSdk = 33
+
+    const val retrofit = "2.9.0"
+    const val okHttp = "4.9.3"
+    const val coroutineTest = "1.6.2"
+
+    const val junit = "5.8.2"
+    const val truth = "1.1.3"
 }
