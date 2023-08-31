f8ca1cc835a5a8f121d5bd1d5938520c6d216495
KwonDae <green201402317@gmail.com>
2023-09-01 00:40:54 +0900

    data - Service 생성

---
+++ data/src/main/java/camp/nextstep/edu/github/data/retrofit/GithubService.kt
@@ -0,0 +1,15 @@
+/**
+ * @author Daewon on 31,August,2023
+ *
+ */
+
+package camp.nextstep.edu.github.data.retrofit
+
+import camp.nextstep.edu.github.data.response.GithubRepositoryResponse
+import retrofit2.http.GET
+
+
+interface GithubService {
+    @GET("repositories")
+    suspend fun getRepositories(): List<GithubRepositoryResponse>
+}
