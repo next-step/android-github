6438cdaee213638c8c8e9aca910fb6a503dd9034
KwonDae <green201402317@gmail.com>
2023-09-01 00:43:01 +0900

    data - 네트워크 에러 핸들링 로직 생성

---
+++ data/src/main/java/camp/nextstep/edu/github/data/retrofit/NetworkResult.kt
@@ -0,0 +1,15 @@
+/**
+ * @author Daewon on 22,April,2023
+ *
+ */
+
+package camp.nextstep.edu.github.data.retrofit
+
+import camp.nextstep.edu.github.domain.Result
+
+
+internal inline fun <T> networkResult(transform: () -> T): Result<T> = try {
+    Result.Success(transform.invoke())
+} catch (e: Exception) {
+    Result.Error(e)
+}
