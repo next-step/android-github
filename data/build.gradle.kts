4b9453ded3091a304a7af03e72803c679208d67c
김영욱 <0.uk.kim@navercorp.com>
2023-09-01 08:41:46 +0900

    junit 테스트 의존성 추가

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -14,4 +14,7 @@ dependencies {
 	implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 	implementation("com.squareup.okhttp3:okhttp:4.9.3")
 	implementation("com.squareup.okhttp3:logging-interceptor:4.9.3")
+
+	testImplementation("junit:junit:4.13.2")
+	testImplementation("com.google.truth:truth:1.1.3")
 }
