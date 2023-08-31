f9ebc60e51ab4a6bed2d421f08cdc59b8def1cea
김영욱 <0.uk.kim@navercorp.com>
2023-09-01 08:36:49 +0900

    Retrofit, OkHttp3 의존성 추가

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -9,4 +9,9 @@ java {
 
 dependencies {
 	implementation(project(":domain"))
+
+	implementation("com.squareup.retrofit2:retrofit:2.9.0")
+	implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+	implementation("com.squareup.okhttp3:okhttp:4.9.3")
+	implementation("com.squareup.okhttp3:logging-interceptor:4.9.3")
 }
