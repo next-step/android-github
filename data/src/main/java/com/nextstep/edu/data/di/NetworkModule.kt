e2c698fc718e3901dfb96f0314ec0b990d982747
duru.hi <hwanggyuil@gmail.com>
2023-09-01 00:17:32 +0900

    feat. data & domain 모듈 hilt 세팅 추가

--- data/src/main/java/com/nextstep/edu/data/di/NetworkModule.kt
+++ data/src/main/java/com/nextstep/edu/data/di/NetworkModule.kt
@@ -2,19 +2,36 @@ package com.nextstep.edu.data.di
 
 import com.nextstep.edu.data.ApiClient
 import com.nextstep.edu.data.GithubService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
-internal object NetworkModule {
 
-    fun provideRetrofit(): Retrofit = Retrofit.Builder()
+@Module
+@InstallIn(SingletonComponent::class)
+internal class NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient
+    ): Retrofit = Retrofit.Builder()
         .baseUrl(ApiClient.BASE_URL)
-        .client(provideOkHttpClient())
+        .client(okHttpClient)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
-    private fun provideOkHttpClient(): OkHttpClient = OkHttpClient.Builder().build()
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient = OkHttpClient.Builder().addInterceptor(HttpLoggingInterceptor()).build()
 
+    @Singleton
+    @Provides
     fun provideGithubService(retrofit: Retrofit): GithubService = retrofit.create(GithubService::class.java)
 }
--- data/src/main/java/com/nextstep/edu/data/repository/RemoteRepositoryImpl.kt
+++ data/src/main/java/com/nextstep/edu/data/repository/RemoteRepositoryImpl.kt
@@ -4,10 +4,12 @@ import com.nextstep.edu.data.GithubService
 import com.nextstep.edu.data.model.toDomain
 import com.nextstep.edu.domain.model.Repository
 import com.nextstep.edu.domain.repository.RemoteRepository
+import javax.inject.Inject
 
-internal class RemoteRepositoryImpl(
+internal class RemoteRepositoryImpl @Inject constructor(
     private val githubService: GithubService
 ): RemoteRepository {
+
     override suspend fun getRepositories(): List<Repository> {
         return githubService.getRepositories().toDomain()
     }
