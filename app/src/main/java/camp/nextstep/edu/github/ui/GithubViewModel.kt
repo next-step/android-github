34f7e884f999c733899da02c33e46830550bf032
duru.hi <hwanggyuil@gmail.com>
2023-09-02 02:40:55 +0900

    fix. step3 피드백 반영(Event 래핑 -> SingleLiveEvent 수정)

--- app/src/main/java/camp/nextstep/edu/github/ui/GithubViewModel.kt
+++ app/src/main/java/camp/nextstep/edu/github/ui/GithubViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import camp.nextstep.edu.github.util.Event
+import camp.nextstep.edu.github.util.SingleLiveEvent
 import com.nextstep.edu.domain.model.Repository
 import com.nextstep.edu.domain.usecase.GetRepositoryUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -16,8 +16,8 @@ class GithubViewModel @Inject constructor(
     private val getRepositoryUseCase: GetRepositoryUseCase
 ): ViewModel() {
 
-    private val _errorEvent: MutableLiveData<Event<Unit>> = MutableLiveData()
-    val errorEvent: LiveData<Event<Unit>> = _errorEvent
+    private val _errorEvent: SingleLiveEvent<Unit> = SingleLiveEvent()
+    val errorEvent: LiveData<Unit> = _errorEvent
 
     private val _repositories: MutableLiveData<List<Repository>> = MutableLiveData()
     val repositories: LiveData<List<Repository>> = _repositories
@@ -26,7 +26,7 @@ class GithubViewModel @Inject constructor(
         viewModelScope.launch {
             getRepositoryUseCase()
                 .onSuccess { _repositories.value = it }
-                .onFailure { _errorEvent.value = Event(Unit) }
+                .onFailure { _errorEvent.value = Unit }
         }
 
     }
