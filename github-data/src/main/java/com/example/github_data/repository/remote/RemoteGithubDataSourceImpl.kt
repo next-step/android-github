759b5b645d4da25ebe01f0c74a8be58d6b341061
OYJ <oyj7677@gmail.com>
2023-09-12 00:55:27 +0900

    제거된 코드 정리

--- github-data/src/main/java/com/example/github_data/repository/remote/RemoteGithubDataSourceImpl.kt
+++ github-data/src/main/java/com/example/github_data/repository/remote/RemoteGithubDataSourceImpl.kt
@@ -3,6 +3,7 @@ package com.example.github_data.repository.remote
 import com.example.github_data.GitHubApi
 import com.example.github_data.GitHubService
 import com.example.github_data.room.GithubRepoEntity
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
@@ -11,6 +12,7 @@ internal class RemoteGithubDataSourceImpl(githubService: GitHubApi? = null): Rem
     // retrofit통신으로 데이터 받아옴.
     private val gitHubService = githubService ?: GitHubService.getGitHubService()
 
+    @OptIn(DelicateCoroutinesApi::class)
     override suspend fun getGitHubRepo(): List<GithubRepoEntity> {
         return GlobalScope.async(Dispatchers.IO) {
             val response = gitHubService.getGitHubInfo()
